4e085c096b98b5300cf61ef82d4ce6f06603eff7
Kaiha-hue <tiandaokuaiyi@gmail.com>
2024-11-04 09:20:29 +0900

    課題終了後

--- src/main/java/raisetech/student/management/StudentManagementApplication.java
+++ src/main/java/raisetech/student/management/StudentManagementApplication.java
@@ -1,20 +1,38 @@
 package raisetech.student.management;
 
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	private String name = "Kai Hatashima";
+	private String name1 = "Hatashima Kai";
+	private String age = "24";
+	private String age1 ="15";
+	
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello, World!";
+	@GetMapping("/studentInfo")
+	public String getStudentInfo() {
+		return name + " " + age + "歳";
+	}
+
+	@PostMapping("/studentInfo")
+	public void setStudentInfo(String name, String age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	@PostMapping("/studentName")
+	public void updateStudentName(String name) {
+		this.name = name;
 	}
 }
