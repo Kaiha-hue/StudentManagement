1dd0acd1a7b5f206b8e9e6080d616df23c74f719
Kaiha-hue <tiandaokuaiyi@gmail.com>
2024-11-28 22:32:28 +0900

    スタンダード第10回課題

--- src/main/java/raisetech/student/management/StudentManagementApplication.java
+++ src/main/java/raisetech/student/management/StudentManagementApplication.java
@@ -1,12 +1,14 @@
 package raisetech.student.management;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.student.management.data.Student;
+import raisetech.student.management.data.StudentsCourses;
+import raisetech.student.management.repository.StudentRepository;
 
 @SpringBootApplication
 @RestController
@@ -25,10 +27,8 @@ public class StudentManagementApplication {
 		return repository.search();
 	}
 
-	@GetMapping("/students")
-	public List<String> getAllStudents() {
-		return repository.findAllStudents().stream()
-				.map(students -> students.getName() + " " + students.getAge())
-				.collect(Collectors.toList());
+	@GetMapping("/studentsCourseList")
+	public List<StudentsCourses> getStudentsCourseList() {
+		return repository.searchStudentsCourses();
 	}
 }
--- src/main/java/raisetech/student/management/data/Student.java
+++ src/main/java/raisetech/student/management/data/Student.java
@@ -1,26 +1,16 @@
-package raisetech.student.management;
+package raisetech.student.management.data;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class Student {
-
-  @JsonProperty("student_id")
   private String studentId;
-
   private String name;
-
   private String nickname;
-
-  @JsonProperty("email_address")
   private String emailAddress;
-
   private String address;
-
   private int age;
-
   private String gender;
 }
