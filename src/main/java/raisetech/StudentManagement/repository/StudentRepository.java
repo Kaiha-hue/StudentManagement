6b99eff4b2126dd09fa135e7c50fd716ce2740de
Kaiha-hue <tiandaokuaiyi@gmail.com>
2024-12-31 16:37:53 +0900

    Javaエンジニアスタンダード 第15回の課題内容に修正しました。

--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -1,6 +1,7 @@
 package raisetech.StudentManagement.repository;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import raisetech.StudentManagement.data.Student;
@@ -24,4 +25,8 @@ public interface StudentRepository {
 
   @Select("SELECT * FROM students_courses")
   List<StudentsCourses> searchStudentsCourses();
+
+  @Insert("INSERT INTO students (id, name, nickname, email, address, age, gender, remark, is_deleted) " +
+      "VALUES (#{id}, #{name}, #{nickname}, #{email}, #{address}, #{age}, #{gender}, #{remark}, false)")
+  void insertStudent(Student student);
  }
